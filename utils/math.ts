ebd4a8649a0561de8aa4486556b82cc30e06b02d
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-04-27 19:56:14 +0800

    Add calcEndPoint and getLinesFromPoints functions

--- utils/math.ts
+++ utils/math.ts
@@ -1,13 +1,32 @@
-import { Line } from "./coordinates";
+import { Line, Point, Pose } from "./coordinates";
 
 export class MathHelper {
-  public static intersects = (l1: Line, l2: Line) => {};
-  public static intersectionPoint = (l1: Line, l2: Line) => {};
   public static degToRad = (deg: number) => {
     return (deg * Math.PI) / 180;
   };
 
   public static radToDeg = (rad: number) => {
     return (rad * 180) / Math.PI;
+  };
+
+  // Distance must be measured in pixels
+  public static calcEndPoint = (pose: Pose, distance: number) => {
+    const { x, y, theta } = pose.unpack();
+
+    return new Point(
+      distance * Math.cos(theta) + x,
+      distance * Math.sin(theta) + y
+    );
+  };
+
+  // To retrieve all the Lines that make up a polygon from a set of points
+  public static getLinesFromPoints = (points: Point[]): Line[] => {
+    return points.reduce((acc, curr, i) => {
+      let endPoint = points[i + 1];
+      if (i === points.length - 1) {
+        endPoint = points[0];
+      }
+      return acc.concat(new Line(curr, endPoint));
+    }, [] as Line[]);
   };
 }
