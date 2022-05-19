b685762873095ef27741c185957901b074ea8bc7
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-05-19 20:43:08 +0800

    Add pre-defined goals to the Map class and MapTemplate

--- game/map.ts
+++ game/map.ts
@@ -1,4 +1,6 @@
+import { get } from "lodash";
 import { Point } from "../utils/coordinates";
+import { Goal } from "./goal";
 import { DynamicObstacle, PolygonObstacle } from "./obstacles";
 
 export type MapTemplate = {
@@ -8,6 +10,7 @@ export type MapTemplate = {
   robotStartPositions: Point[]; // Starting positions for the robots, must be same length as robotCount variable
   staticObstacles: PolygonObstacle[];
   dynamicObstacles?: DynamicObstacle[]; // A map may or may not have dynamic obstacles
+  goals?: Goal[]; // A map may or may not have goals
 };
 
 export class Map {
@@ -17,6 +20,7 @@ export class Map {
   private robotStartPositions: Point[];
   private width: number;
   private height: number;
+  private goals?: Goal[];
 
   constructor(map: MapTemplate) {
     this.robotCount = map.robotCount;
@@ -24,6 +28,7 @@ export class Map {
     this.staticObstacles = map.staticObstacles;
     this.width = map.width;
     this.height = map.height;
+    this.goals = map.goals;
   }
 
   public unpack = () => {
@@ -34,6 +39,11 @@ export class Map {
       dynamicObstacles: this.dynamicObstacles,
       width: this.width,
       height: this.height,
+      goals: this.goals,
     };
+  };
+
+  public getGoal = (idx: number) => {
+    return get(this.goals, idx, undefined);
   };
 }
