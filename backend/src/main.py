1f8cc1f9df42c5ddfdc7deaf1e3a483cb679379e
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-05-24 16:47:55 +0800

    Add the algorithm API to the main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,34 @@
-from typing import Union
+from typing import List, Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.models import Algorithm
+
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/algorithm/")
+def algorithm(algorithm: Algorithm):
+    print("Robots \n", algorithm.robots)
+    print("Environment \n", algorithm.environment)
+    return "Test API Successful"
