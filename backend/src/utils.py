a91c211a16cfedfe3a64d5bc25f6fe32d93bbece
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-05-30 23:05:14 +0800

    Add and refactor some util functions

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,4 +1,8 @@
-import settings
+from math import cos, sin
+from src.api_models import _Robot
+from models.point import Point
+from models.pose import Pose
+import src.settings as settings
 
 
 def uni_to_diff(velocity, omega):
@@ -12,4 +16,21 @@ def uni_to_diff(velocity, omega):
     velocityL = ((2.0 * velocity) - (omega * length)) / (2.0 * radius)
     velocityR = ((2.0 * velocity) + (omega * length)) / (2.0 * radius)
     
+    # Not sure whether we allow the robot wheel to reverse, therefore, any negative drive rates will be set to 0.
+    # TODO: Might have to remove this restriction, don't know what effects it has on the error terms in the calculation of steering inputs.
+    if (velocityL < 0):
+        velocityL = 0
+    if (velocityR < 0):
+        velocityR = 0
+
     return velocityL, velocityR
+
+
+def transform_robot_api_model(robot: _Robot):
+    """Transforms the _Robot API model to return a Robot class"""
+    pose: Pose = Pose(Point(robot.pose.vector.x, robot.pose.vector.y), robot.pose.theta)
+    if (robot.current_goal is not None):
+        current_goal: Point = Point(robot.current_goal.x, robot.current_goal.y)
+    else:
+        current_goal = None
+    return robot.id, pose, robot.sensor_readings, current_goal, robot.pid_metadata
