b1abc78c09288c2fa7971ef78a0bda43975c5166
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-05-30 22:58:18 +0800

    Rename models to api_models.py

---
+++ backend/src/api_models.py
@@ -0,0 +1,39 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class _Point(BaseModel):
+    x: float
+    y: float
+
+
+class _Pose(BaseModel):
+    vector: _Point
+    theta: float # heading
+
+
+class _SensorReading(BaseModel):
+    reading: _Point
+
+
+class _PIDMetadata(BaseModel):
+    prev_eP: float
+    prev_eI: float
+
+
+class _Robot(BaseModel):
+    id: int
+    pose: _Pose
+    sensor_readings: List[_SensorReading]
+    current_goal: _Point = None # optional, a robot can not have a goal at a given time
+    pid_metadata: _PIDMetadata # optional, pid metadata for the robot
+
+
+class _TopologicalEnvironment(BaseModel):
+    width: int
+    height: int
+
+
+class _Algorithm(BaseModel):
+    robots: List[_Robot]
+    environment: _TopologicalEnvironment
