1ad7e44d0e5d07616404d9ab6e07a3dee2bef5f3
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-06-09 14:13:33 +0800

    Make changes to Canvas component

--- components/Canvas/index.tsx
+++ components/Canvas/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { CanvasProp } from "./props";
 import { CanvasHelper } from "../../utils/canvas";
 import { Simulator, SimulatorAction } from "../../game";
@@ -31,12 +31,23 @@ const Canvas = (props: CanvasProp) => {
     undefined
   );
   const [simulatorAction, setSimulatorAction] = useState<SimulatorAction>(
-    SimulatorAction.NAVIGATION
+    SimulatorAction.MAPPING
   ); // First simulator action state is MAPPING procedure
+  const [currentRobotId, setCurrentRobotId] = useState<number>(0); // Current robot id that is being controlled
 
   // Initialize map json data
-  const response = executeInitializeMapJSON(simulator.generatePayload());
-  response.then((res) => console.log(res));
+  // TODO: Refactor this part
+  // const response = executeInitializeMapJSON(simulator.generatePayload());
+  // response.then((res) => console.log(res));
+
+  // const nextRobotId = useCallback(() => {
+  //   setCurrentRobotId((currentRobotId) => {
+  //     if (currentRobotId + 1 === simulator.getRobots().length) {
+  //       return 0;
+  //     }
+  //     return currentRobotId + 1;
+  //   });
+  // }, [simulator]);
 
   // ========================= ENVIRONMENT RENDERING =========================
   // Declare canvas references
@@ -94,6 +105,11 @@ const Canvas = (props: CanvasProp) => {
       // Check for robot goals
       simulator.checkRobotGoals();
 
+      // Check for mapping
+      if (simulatorAction === SimulatorAction.MAPPING) {
+        simulator.mapping();
+      }
+
       // Execute algorithm
       // TODO: Uncomment once it is debugged
       // const response = executeBatchAlgorithm(simulator.generatePayload());
@@ -101,12 +117,15 @@ const Canvas = (props: CanvasProp) => {
       // response.then((res) => simulator.execute(res)).catch(() => {});
 
       robots[0].drive(10, 0);
+
+      // nextRobotId();
+
       // robots[1].drive(5, 5);
     }, TICKS_PER_UPDATE);
 
     // Unmount ticker
     return () => clearInterval(ticker);
-  }, [simulator]);
+  }, [simulator, simulatorAction]);
 
   // This useEffect hook will act as the sensor reading loop for the robots
   useEffect(() => {
@@ -161,7 +180,7 @@ const Canvas = (props: CanvasProp) => {
   useEffect(() => {
     switch (simulator.getAction()) {
       case SimulatorAction.MAPPING:
-        setSimulatorAction(SimulatorAction.NAVIGATION);
+        setSimulatorAction(SimulatorAction.MAPPING);
         break;
 
       case SimulatorAction.MAPPING_COMPLETE:
@@ -169,7 +188,7 @@ const Canvas = (props: CanvasProp) => {
         break;
 
       case SimulatorAction.NAVIGATION:
-        setSimulatorAction(SimulatorAction.MAPPING);
+        setSimulatorAction(SimulatorAction.NAVIGATION);
         break;
 
       default:
@@ -179,6 +198,7 @@ const Canvas = (props: CanvasProp) => {
 
   useEffect(() => {
     if (simulatorAction === SimulatorAction.MAPPING_COMPLETE) {
+      // Navigate to leader robot position
       const response = executeGenerateMap({});
       response.then(() => simulator.mapGenerated());
     }
