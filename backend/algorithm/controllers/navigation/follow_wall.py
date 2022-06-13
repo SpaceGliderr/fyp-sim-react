45e6611380cdbb41518d1b1193998cfaddb979db
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-06-14 03:39:37 +0800

    Add PID controller to Follow Wall controller

--- backend/algorithm/controllers/navigation/follow_wall.py
+++ backend/algorithm/controllers/navigation/follow_wall.py
@@ -1,11 +1,16 @@
 from models.point import Point
 from models.pose import Pose
 from src.api_models import _PIDMetadata
+import src.settings as settings
 
 
 class FollowWall:
     def __init__(self, pose: Pose, pid_metadata: _PIDMetadata, sensor_readings) -> None:
         self.pose = pose
+
+        self.kP = settings.PID_CONTROLLER['kP'] # Proportional gain.
+        self.kI = settings.PID_CONTROLLER['kI'] # Integral gain.
+        self.kD = settings.PID_CONTROLLER['kD'] # Derivative gain.
 
 
     def reset_pid(self):
