877004cb10d672df1a5ebb448e0f44f7e9902d94
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-06-24 10:33:43 +0800

    Add Robot Worker logic

--- public/workers/robot.js
+++ public/workers/robot.js
@@ -4,25 +4,75 @@ const API_HEADERS = {
 };
 
 const executePathPlanning = async (payload) => {
-  const response = await fetch(`http://127.0.0.1:8000/plan_path/`, {
+  const response = await fetch(`http://127.0.0.1:800${payload.id}/plan_path/`, {
     method: "POST",
     headers: API_HEADERS,
-    body: payload,
+    body: JSON.stringify(payload),
   });
   return await response.json();
 };
 
-self.onmessage = (event) => {
+const executeSingleRobot = async (payload, id) => {
+  const response = await fetch(
+    `http://127.0.0.1:800${payload.id}/single_robot/`,
+    {
+      method: "POST",
+      headers: API_HEADERS,
+      body: JSON.stringify(payload),
+    }
+  );
+  return await response.json();
+};
+
+self.onmessage = async (event) => {
   const { data } = event;
-  const test = JSON.parse(data);
-  console.log(`Worker for robot ID ${test.id} started`);
+  const args = JSON.parse(data);
 
-  const response = executePathPlanning(data);
+  const { operation, payload } = args;
 
-  response.then((res) => {
-    console.log(`Response for robot ID ${test.id} received`);
-    console.log(res);
+  let response;
+
+  switch (operation) {
+    case "PLAN_PATH":
+      console.log("Plan Path called");
+      await executePathPlanning(payload)
+        .then((res) => {
+          console.log(`Response received`);
+          console.log(res);
+          response = res;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      break;
+
+    case "FIND_LEADER":
+      await executeSingleRobot(payload).then((res) => {
+        console.log(`Response received`);
+        console.log(res);
+        response = res;
+      });
+
+      break;
+
+    case "NAVIGATE":
+      await executeSingleRobot(payload).then((res) => {
+        console.log(`Response received`);
+        console.log(res);
+        response = res;
+      });
+
+      break;
+
+    default:
+      console.log("No such operation found");
+      break;
+  }
+  console.log(response);
+
+  self.postMessage({
+    operation,
+    payload: response,
   });
-
-  self.postMessage(`Robot ${data} has finished executing`);
 };
