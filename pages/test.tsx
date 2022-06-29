4c84be6ee93aaffac15ffe8dad4b03e3e0b9fe7e
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-06-29 17:55:57 +0800

    Test functions

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
 import { useMemo, useState } from "react";
 import { Simulator } from "../game";
+import { Goal, GoalShape } from "../game/goal";
 import { Map } from "../game/map";
 import { MAP_1 } from "../maps/map_1";
+import { MAP_2 } from "../maps/map_2";
+import { MAP_3 } from "../maps/map_3";
+import { Point } from "../utils/coordinates";
 
 const Test: NextPage = () => {
   // Declare selected map
-  const map = useMemo(() => new Map(MAP_1), []);
+  const map = useMemo(() => new Map(MAP_3), []);
 
   // Instantiate the simulator class based on the chosen map
   const simulator = useMemo(() => new Simulator(map), [map]);
@@ -27,13 +31,53 @@ const Test: NextPage = () => {
   //   // setRes(data);
   // };
 
+  // const executeAlgorithm = async () => {
+  //   const response = await fetch(`${process.env.API_URL}/clear_map_json/`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   console.log("🚀 ~ file: test.tsx ~ line 6 ~ executeAlgorithm ~ data", data);
+  //   // setRes(data);
+  // };
+
+  // const executeAlgorithm = async () => {
+  //   const response = await fetch(
+  //     `${process.env.API_URL}/generate_ground_truth_map/`,
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "Access-Control-Allow-Origin": "*",
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(map.getGroundTruthPayload()),
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log("🚀 ~ file: test.tsx ~ line 6 ~ executeAlgorithm ~ data", data);
+  //   // setRes(data);
+  // };
+
   const executeAlgorithm = async () => {
-    const response = await fetch(`${process.env.API_URL}/clear_map_json/`, {
+    const robot = simulator.getRobotById(1);
+    robot.setCurrentGoal(
+      new Goal([new Point(680, 17)], GoalShape.CIRCLE, 0, 7, new Date())
+    );
+    const response = await fetch(`${process.env.API_URL}/test_plan_path/`, {
       method: "POST",
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
+      body: JSON.stringify({
+        robot: robot.generatePayload(),
+        mapping: simulator
+          .getLeaderRobot()
+          .generateMappingPayload(simulator.getWidth(), simulator.getHeight()),
+      }),
     });
     const data = await response.json();
     console.log("🚀 ~ file: test.tsx ~ line 6 ~ executeAlgorithm ~ data", data);
